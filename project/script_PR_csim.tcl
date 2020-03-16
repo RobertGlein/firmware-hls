dec38575302d7478e3625bb361a3b95c002e3626
Robert Glein <robert.glein@colorado.edu>
2020-03-16 17:19:06 -0600

    first CI try

---
+++ project/script_PR_csim.tcl
@@ -0,0 +1,31 @@
+# Script to generate project for PR
+#   vivado_hls -f script_PR.tcl
+#   vivado_hls -p projrouter
+# WARNING: this will wipe out the original project by the same name
+
+# create new project (deleting any existing one of same name)
+open_project -reset PR
+
+# source files
+set CFLAGS {-std=c++11 -I../TrackletAlgorithm}
+set_top ProjectionRouterTop
+add_files ../TrackletAlgorithm/ProjectionRouterTop.cpp -cflags "$CFLAGS"
+add_files -tb ../TestBenches/ProjectionRouter_test.cpp -cflags "$CFLAGS"
+
+# data files
+add_files -tb ../emData/PR/PR_L3PHIC/
+#add_files -tb ../emData/PR/PR_L3L4_L1PHI3/
+
+open_solution "solution1"
+
+# Define FPGA, clock frequency & common HLS settings.
+source settings_hls.tcl
+
+csim_design -compiler gcc -mflags "-j8"
+#csynth_design
+#cosim_design 
+#export_design -format ip_catalog
+# Adding "-flow impl" runs full Vivado implementation, providing accurate resource use numbers (very slow).
+#export_design -format ip_catalog -flow impl
+
+exit
--- project/script_PR_csynth_cosim_export.tcl
+++ project/script_PR_csynth_cosim_export.tcl
@@ -4,7 +4,7 @@
 # WARNING: this will wipe out the original project by the same name
 
 # create new project (deleting any existing one of same name)
-open_project -reset projrouter
+open_project PR
 
 # source files
 set CFLAGS {-std=c++11 -I../TrackletAlgorithm}
@@ -21,7 +21,7 @@ open_solution "solution1"
 # Define FPGA, clock frequency & common HLS settings.
 source settings_hls.tcl
 
-csim_design -compiler gcc -mflags "-j8"
+#csim_design -compiler gcc -mflags "-j8"
 csynth_design
 cosim_design 
 export_design -format ip_catalog
